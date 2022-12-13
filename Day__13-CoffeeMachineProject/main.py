387ca289915d67e5ea2ba9b61db361a8e3ecefab
vardineee <varduhi.poghosian@gmail.com>
2022-12-13 22:25:52 +0400

    coffee machine project

--- Day__13-CoffeeMachineProject/main.py
+++ Day__13-CoffeeMachineProject/main.py
@@ -1,34 +1,5 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from menu_data import MENU, resources
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
 water = resources['water']
 milk = resources["milk"]
 coffee = resources["coffee"]

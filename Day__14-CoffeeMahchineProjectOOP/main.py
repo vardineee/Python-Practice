4a4136b18db909a8f75fef2c01d2bfb1ee529da5
Varduhi Poghosyan <83503972+vardineee@users.noreply.github.com>
2022-12-16 23:38:45 +0400

    Delete Day__14-CoffeeMahchineProjectOOP directory

--- Day__14-CoffeeMahchineProjectOOP/main.py
+++
@@ -1,8 +0,0 @@
-from prettytable import PrettyTable
-
-table = PrettyTable()
-column = table.add_column("City name", ["Adelaide", "Brisbane", "Darwin", "Hobart", "Sydney", "Melbourne", "Perth"])
-column_1 = table.add_column("Area", [1295, 5905, 112, 1357, 2058, 1566, 5386])
-table.align = 'r'
-
-print(table)
